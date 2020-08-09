7b2cdf76f5e6255a38c4115a763dc7368fb6b379
Tomás Accini <taccini@fb.com>
2020-08-09 02:06:40 -0300

    Exit qr handling (#7)
    * change schema and model from QR to Space
    
    * add double qr if exitQR is true
    
    * chance exitQR to hasExit

--- src/models/handlers/EstablishmentHandler.js
+++ src/models/handlers/EstablishmentHandler.js
@@ -35,7 +35,7 @@ module.exports = function EstablishmentHandler() {
         _id: new mongoose.Types.ObjectId(),
         name: _space.name,
         m2: _space.m2,
-        exitQR: _space.exitQR,
+        hasExit: _space.hasExit,
         openPlace: _space.openPlace,
         establishmentId: establishmentData._id
       };
@@ -61,7 +61,12 @@ module.exports = function EstablishmentHandler() {
     let PDFInfo = [];
     for (const space_id of establishment.spaces) {
       let current_space = await SpaceHandler().findSpace(space_id);
-      PDFInfo.push([current_space.name, space_id.toString()]);
+      if (current_space.hasExit) {
+        PDFInfo.push([`${current_space.name} Entrada`, space_id.toString()]);
+        PDFInfo.push([`${current_space.name} Salida`, `${space_id.toString()}_exit`]);
+      } else {
+        PDFInfo.push([current_space.name, space_id.toString()]);
+      }
     }
     return PDFInfo;
   };
