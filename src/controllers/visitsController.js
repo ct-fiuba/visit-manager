ece6c7eda64870dcfaa2543731837607e4db9917
Tomás Accini <taccini@fb.com>
2020-11-02 23:17:05 -0300

    separate logic into service

--- src/controllers/visitsController.js
+++ src/controllers/visitsController.js
@@ -1,4 +1,6 @@
 module.exports = function visitsController(visitHandler) {
+  const visitRegistration = require('../services/visitRegistration');
+
   const errorDB = (res, err) => {
     console.log(err.message);
     return res.status(500).json({ reason: 'DB Error' });
@@ -12,23 +14,7 @@ module.exports = function visitsController(visitHandler) {
   };
 
   const add = async (req, res, next) => {
-    return visitHandler.visitExists(req.body)
-      .then(visit => {
-        if (visit) {
-          return res.status(409).json({ reason: 'Visit already registered' });
-        }
-        return visitHandler.spaceExists(req.body.scanCode)
-          .then(space => {
-            if (!space) {
-              return res.status(404).json({ reason: 'Space linked to the scan code not found' });
-            }
-            return visitHandler.addVisit(req.body)
-              .then(visit => res.status(201).json({ _id: visit._id }))
-              .catch(err => errorDB(res, err));
-          })
-          .catch(err => errorDB(res, err));
-      })
-      .catch(err => errorDB(res, err));
+    return await visitRegistration(visitHandler).registerVisit(req, res);
   };
 
   return {
