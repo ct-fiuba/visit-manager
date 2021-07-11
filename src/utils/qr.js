920ae3b223002bdc9f7fb87e6a2afdc1ac11549c
Oli <olifer97@gmail.com>
2021-07-11 17:46:45 -0300

    fix in pdf title

--- src/utils/qr.js
+++ src/utils/qr.js
@@ -1,6 +1,6 @@
 module.exports = {
     getQRInfo: (establishmentName, spaceName, id, isExit) => ({
-  title: `Establecimiento: ${establishmentName} \nEspacio: ${spaceName} (Entrada)`,
+  title: `Establecimiento: ${establishmentName} \nEspacio: ${spaceName} (${isExit ? 'Salida' : 'Entrada'})`,
   code: { name: establishmentName, space: spaceName, id: id.toString(), isExit }
 })
 };
